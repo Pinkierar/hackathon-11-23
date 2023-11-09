7fd6e8b983354a8742bc98ceb986728f73e9bbe7
Pinkierar <admin@pinkierar.ru>
2023-11-09 20:13:32 +0300

    Добавлен global и checkNever

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,12 @@
 // noinspection BadExpressionStatementJS,CommaExpressionJS
 
 import * as process from 'process';
+import express from 'express';
 
 require('dotenv').config();
 
-import express from 'express';
-
 const app = express();
+
 app.listen(Number(process.env.PORT) ?? 5000, '0.0.0.0', () => {
   console.log('started');
 });
