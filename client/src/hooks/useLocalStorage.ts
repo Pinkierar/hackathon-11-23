55e374778f1b9dc1e7a0d4f5ff0cf5a7061068ca
Pinkierar <admin@pinkierar.ru>
2023-11-11 11:46:07 +0300

    Поддержка объектов в хуке useLocalStorage

--- client/src/hooks/useLocalStorage.ts
+++ client/src/hooks/useLocalStorage.ts
@@ -1,17 +1,18 @@
 import {useState} from 'react';
 
-type LocalStorageValue = string | null;
-type LocalStorageSetter = (value: LocalStorageValue) => void;
-type UseLocalStorage = (key: string) => readonly [LocalStorageValue, LocalStorageSetter];
+type LocalStorageValue<T> = T | null;
+type LocalStorageSetter<T> = (value: LocalStorageValue<T>) => void;
+type UseLocalStorageResult<T> = readonly [LocalStorageValue<T>, LocalStorageSetter<T>]
 
-export const useLocalStorage: UseLocalStorage = key => {
-  const [state, setState] = useState<LocalStorageValue>(
-    window.localStorage.getItem(key),
+export const useLocalStorage = <T>(key: string): UseLocalStorageResult<T> => {
+  const oldValue = window.localStorage.getItem(key);
+  const [state, setState] = useState<LocalStorageValue<T>>(
+    oldValue === null ? null : JSON.parse(oldValue),
   );
 
-  const setter: LocalStorageSetter = value => {
+  const setter: LocalStorageSetter<T> = value => {
     if (value === null) window.localStorage.removeItem(key);
-    else window.localStorage.setItem(key, value);
+    else window.localStorage.setItem(key, JSON.stringify(value));
 
     setState(value);
   };
