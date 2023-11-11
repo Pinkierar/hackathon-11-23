54018b817d33ba5844f76a1e231e00d6eee5a6ed
Pinkierar <admin@pinkierar.ru>
2023-11-11 20:08:06 +0300

    Компонент P5Canvas принимает className

--- client/src/components/P5Canvas/index.tsx
+++ client/src/components/P5Canvas/index.tsx
@@ -1,31 +1,27 @@
-import {HTMLAttributes, useEffect, useRef, useState} from 'react';
-import {observer} from 'mobx-react-lite';
-import {Contextual} from '#includes/graphics';
-import P5, {Vector} from 'p5';
+import { HTMLAttributes, useEffect, useRef, useState } from 'react';
+import { observer } from 'mobx-react-lite';
+import { Contextual } from '#includes/graphics';
+import P5, { Vector } from 'p5';
 import style from './style.module.scss';
+import { cl } from '#includes/cl';
 
 type Sketch = {
-  setup?: () => void,
-  draw?: () => void,
+  setup?: () => void;
+  draw?: () => void;
 };
 
 type P5CanvasPropsMin = {
-  children?: never,
-  sketch?: (p: P5Type) => Sketch,
+  children?: never;
+  sketch?: (p: P5Type) => Sketch;
 };
 
-type P5CanvasProps =
-  Omit<HTMLAttributes<HTMLElement>, keyof P5CanvasPropsMin>
-  & P5CanvasPropsMin;
+type P5CanvasProps = Omit<HTMLAttributes<HTMLElement>, keyof P5CanvasPropsMin> &
+  P5CanvasPropsMin;
 
 const size: Vector = new Vector(1, 1);
 
-export const P5Canvas = observer<P5CanvasProps>(props => {
-  const {
-    children,
-    sketch,
-    ...otherProps
-  } = props;
+export const P5Canvas = observer<P5CanvasProps>((props) => {
+  const { children, sketch, className, ...otherProps } = props;
 
   const [p, setP] = useState<P5Type | null>(null);
 
@@ -33,15 +29,15 @@ export const P5Canvas = observer<P5CanvasProps>(props => {
 
   useEffect(() => {
     if (!containerRef.current) return;
-    const container = containerRef.current
+    const container = containerRef.current;
 
     const p5 = new P5((p: P5Type) => {
-      const {draw, setup} = sketch
+      const { draw, setup } = sketch
         ? sketch(p)
-        : ({} as { draw: undefined, setup: undefined });
+        : ({} as { draw: undefined; setup: undefined });
 
       p.setup = () => {
-        p.createCanvas(size.x, size.y, 'p2d');
+        p.createCanvas(size.x, size.y, p.P2D);
         setup && setup();
       };
       p.draw = () => draw && draw();
@@ -63,7 +59,7 @@ export const P5Canvas = observer<P5CanvasProps>(props => {
       if (!containerRef.current) return;
       const container = containerRef.current;
 
-      const {width, height} = container.getBoundingClientRect();
+      const { width, height } = container.getBoundingClientRect();
       const zoom = window.devicePixelRatio;
 
       size.set(width, height).mult(zoom);
@@ -79,6 +75,10 @@ export const P5Canvas = observer<P5CanvasProps>(props => {
   }, [p, containerRef]);
 
   return (
-    <div className={style.container} {...otherProps} ref={containerRef}/>
+    <div
+      className={cl(style.container, className)}
+      {...otherProps}
+      ref={containerRef}
+    />
   );
 });
