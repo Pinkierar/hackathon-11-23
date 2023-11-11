c53479d26549e67e7b9d5a9d8e0e2c183d33d06e
Pinkierar <admin@pinkierar.ru>
2023-11-11 17:53:31 +0300

    Генератор лабиринта полностью взят с https://github.com/dstromberg2/maze-generator/blob/master/mazegenerator.js

--- client/src/includes/Labyrinth/index.ts
+++ client/src/includes/Labyrinth/index.ts
@@ -10,74 +10,59 @@ export class Labyrinth {
   }
 
   private newMaze({x, y}: Vector): Cell[][] {
-    // Установите переменные и стартовую сетку
-    const totalCells = x * y;
-    const cells: Cell[][] = [];
-    const unvisited: boolean[][] = [];
-    for (let i = 0; i < y; i++) {
-      cells[i] = [];
-      unvisited[i] = [];
-      for (let j = 0; j < x; j++) {
-        cells[i][j] = [0, 0, 0, 0];
-        unvisited[i][j] = true;
+    // Establish variables and starting grid
+    var totalCells = x*y;
+    var cells = new Array();
+    var unvis = new Array();
+    for (var i = 0; i < y; i++) {
+      cells[i] = new Array();
+      unvis[i] = new Array();
+      for (var j = 0; j < x; j++) {
+        cells[i][j] = [0,0,0,0];
+        unvis[i][j] = true;
       }
     }
 
-    // Установите случайную позицию, чтобы начать с
-    const currentCell: Vector = new Vector(
-      Math.floor(Math.random() * y),
-      Math.floor(Math.random() * x),
-    );
-    const path = [currentCell];
-    unvisited[currentCell.x][currentCell.y] = false;
-    let visited = 1;
+    // Set a random position to start from
+    var currentCell = [Math.floor(Math.random()*y), Math.floor(Math.random()*x)];
+    var path = [currentCell];
+    unvis[currentCell[0]][currentCell[1]] = false;
+    var visited = 1;
 
-    // Перебрать все доступные позиции ячеек
+    // Loop through all available cell positions
     while (visited < totalCells) {
-      // Определить соседние ячейки
-      const pot: Cell[] = [
-        [currentCell.x - 1, currentCell.y, 0, 2],
-        [currentCell.x, currentCell.y + 1, 1, 3],
-        [currentCell.x + 1, currentCell.y, 2, 0],
-        [currentCell.x, currentCell.y - 1, 3, 1],
-      ];
-      const neighbors: Cell[] = [];
+      // Determine neighboring cells
+      var pot = [[currentCell[0]-1, currentCell[1], 0, 2],
+        [currentCell[0], currentCell[1]+1, 1, 3],
+        [currentCell[0]+1, currentCell[1], 2, 0],
+        [currentCell[0], currentCell[1]-1, 3, 1]];
+      var neighbors = new Array();
 
-      // Определите, находится ли каждая соседняя ячейка в игровой сетке и проверена ли она уже
-      for (let l = 0; l < 4; l++) {
-        if (
-          pot[l][0] > -1 &&
-          pot[l][0] < y &&
-          pot[l][1] > -1 &&
-          pot[l][1] < x &&
-          unvisited[pot[l][0]][pot[l][1]]
-        ) {
-          neighbors.push(pot[l]);
-        }
+      // Determine if each neighboring cell is in game grid, and whether it has already been checked
+      for (var l = 0; l < 4; l++) {
+        if (pot[l][0] > -1 && pot[l][0] < y && pot[l][1] > -1 && pot[l][1] < x && unvis[pot[l][0]][pot[l][1]]) { neighbors.push(pot[l]); }
       }
 
-      // Если найдена хотя бы одна активная соседняя ячейка
+      // If at least one active neighboring cell has been found
       if (neighbors.length) {
-        // Выберите случайным образом одного из соседей
-        const next = neighbors[Math.floor(Math.random() * neighbors.length)];
+        // Choose one of the neighbors at random
+        var next = neighbors[Math.floor(Math.random()*neighbors.length)];
 
-        // Удалить стену между текущей ячейкой и выбранной соседней ячейкой
-        cells[currentCell.x][currentCell.y][next[2]] = 1;
+        // Remove the wall between the current cell and the chosen neighboring cell
+        cells[currentCell[0]][currentCell[1]][next[2]] = 1;
         cells[next[0]][next[1]][next[3]] = 1;
 
-        // Отметьте соседа как посещенного и установите его в качестве текущей ячейки.
-        unvisited[next[0]][next[1]] = false;
+        // Mark the neighbor as visited, and set it as the current cell
+        unvis[next[0]][next[1]] = false;
         visited++;
-        currentCell.set(next[0], next[1]);
+        currentCell = [next[0], next[1]];
         path.push(currentCell);
       }
-      // В противном случае вернитесь на шаг назад и продолжайте идти.
+      // Otherwise go back up a step and keep going
       else {
-        const prev = path.pop();
-        currentCell.set(prev!);
+        currentCell = path.pop()!;
       }
     }
-
     return cells;
   }
 }
